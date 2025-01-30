749e9cdfc610a2af64cc766b7b5252c222f063db
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-01-30 11:40:52 -0500

    renamed variable

--- Day 2/Tip Calculator Project/task.py
+++ Day 2/Tip Calculator Project/task.py
@@ -1,7 +1,7 @@
 print("Welcome to the tip calculator!")
 bill = float(input("What was the total bill? $"))
 tip = int(input("What percentage tip would you like to give? 18 20 22 "))
-total = (tip/100) + 1
+tip_as_percent = (tip/100) + 1
 people = int(input("How many people to split the bill? "))
-price_per_person = round((bill * total) / people, 2)
+price_per_person = round((bill * tip_as_percent) / people, 2)
 print(f"Each person should pay ${price_per_person}.")
