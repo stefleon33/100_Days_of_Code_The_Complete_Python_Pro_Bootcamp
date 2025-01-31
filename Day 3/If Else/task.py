2a30b7883ed51f91e96d7ee7af5e024894366ec0
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-01-31 18:58:06 -0500

    Task 1: if / else statement

--- Day 3/If Else/task.py
+++ Day 3/If Else/task.py
@@ -1,7 +1,7 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
 
-if height > 120:
+if height >= 120:
     print("Enjoy the ride!")
 else:
     print("Sorry, you're not tall enough!")
