7ad06e15cba53d2188c966e09faeb43f99cdd963
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-02-03 10:20:25 -0500

    Task 4: Pizza store work problem

--- Day 3/Python Pizza/task.py
+++ Day 3/Python Pizza/task.py
@@ -2,4 +2,21 @@ print("Welcome to Python Pizza Deliveries!")
 size = input("What size pizza do you want? S, M or L: ")
 pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
 extra_cheese = input("Do you want extra cheese? Y or N: ")
+bill = 0
 
+if size.upper() == "S":
+    bill += 15
+elif size.upper() == "M":
+    bill += 20
+elif size.upper() == "L":
+    bill += 25
+else:
+    print("You typed the wrong inputs.")
+if pepperoni.upper() == "Y":
+    if size == "S":
+        bill += 2
+    else:
+        bill += 3
+if extra_cheese.upper() ==  "Y":
+    bill += 1
+print(f"Your final bill is: ${bill}.")
