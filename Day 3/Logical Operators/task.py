a6bca9046718135a490aeef645144d6c6449978b
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-02-03 10:38:11 -0500

    Task 5: logical operators, using and in an if statement

--- Day 3/Logical Operators/task.py
+++ Day 3/Logical Operators/task.py
@@ -6,13 +6,17 @@ if height >= 120:
     print("You can ride the rollercoaster!")
     age = int(input("What is your age? "))
     if age < 12:
-        bill = 5
+        bill += 5
         print("Child tickets are $5.")
     elif age <= 18:
-        bill = 7
+        bill += 7
         print("Youth tickets are $7.")
+    elif age >= 45 and age <= 55:
+        #45 <= age <= 55
+        #No need to modify the bill since it is free.
+        print("Midlife crisis tickets are free!")
     else:
-        bill = 12
+        bill += 12
         print("Adult tickets are $12.")
 
     wants_photo = input("Do you want a photo taken? Y or N. ")
