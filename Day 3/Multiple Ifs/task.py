9baa0fb8a45ecba0ae774c92ff92f5d3a089b53d
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-02-03 09:56:40 -0500

    Task 3: Multiple If statements must be at the same indentation

--- Day 3/Multiple Ifs/task.py
+++ Day 3/Multiple Ifs/task.py
@@ -1,14 +1,25 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
+bill = 0
 
 if height >= 120:
     print("You can ride the rollercoaster")
     age = int(input("What is your age? "))
     if age <= 12:
-        print("Please pay $5.")
+        bill = 5
+        print("Your ticket will be $5.")
     elif age <= 18:
-        print("Please pay $7.")
+        bill = 7
+        print("Your ticket will be $7.")
     else:
-        print("Please pay $12.")
+        bill = 12
+        print("Your ticket will be $12.")
+    wants_photo = input("Do you want a photo of your ride? Type y for Yes or n for No. ")
+
+    if wants_photo.lower() == "y":
+        #Add $3 to the bill
+        bill += 3
+
+    print(f"Your final bill is ${bill}.")
 else:
     print("Sorry you have to grow taller before you can ride.")
