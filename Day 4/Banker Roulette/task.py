d5e893d5c05c3cd9a048286a59bc87a224e68a7b
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-02-04 10:18:52 -0500

    Day 4: Banker roulette challenge

--- Day 4/Banker Roulette/task.py
+++ Day 4/Banker Roulette/task.py
@@ -1,1 +1,22 @@
+import random
+
+#My solution
 friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+random_pick = random.randint(0,4)
+print(random_pick)
+if random_pick == 0:
+    print(friends[0])
+elif random_pick == 1:
+    print(friends[1])
+elif random_pick == 2:
+    print(friends[2])
+elif random_pick == 3:
+    print(friends[3])
+else:
+    print(friends[4])
+
+#Given solution 1
+print(random.choice(friends))
+
+#Given solution 2
+print(friends[random_pick])
