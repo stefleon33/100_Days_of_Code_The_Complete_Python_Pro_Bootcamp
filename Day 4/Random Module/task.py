179df5ce112a5666c8348304087413f7e4f763af
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-02-04 09:46:33 -0500

    Day 4: random number options

---
+++ Day 4/Random Module/task.py
@@ -0,0 +1,24 @@
+import random
+
+#random_integer = random.randint(1, 10)
+#print(random_integer)
+
+#random_number_0_to_1 = random.random() * 10
+#print(random_number_0_to_1)
+
+#random_float = random.uniform(1 ,10)
+#print(random_float)
+
+#random = round(random.random() * 10, 2)
+#print(random)
+#if random >= 5:
+    #print("heads")
+#else:
+    #print("tails")
+
+random_heads_or_tails = random.randint(0, 1)
+print(random_heads_or_tails)
+if random_heads_or_tails == 0:
+    print("heads")
+else:
+    print("tails")
