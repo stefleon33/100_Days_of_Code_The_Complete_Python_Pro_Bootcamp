f5ab2504c167a5556210f445bd2c98eef12f5d3b
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-02-10 10:04:02 -0500

    Day 8: Learning about functions with inputs

---
+++ Day 8/Functions with Inputs/task.py
@@ -0,0 +1,14 @@
+# def greet():
+#     print("Hello, ")
+#     print("How is your day today? ")
+#     print("What are you doing today? ")
+#
+# greet()
+
+#Function that allows for inputs
+def greet_with_name(name):
+    print(f"Hello, {name}")
+    print(f"How is your day today {name}? ")
+    print(f"What are you doing today {name}? ")
+
+greet_with_name("Stef")
