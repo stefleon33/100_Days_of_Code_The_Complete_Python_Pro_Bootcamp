acaa5a73c123f99eb2881e02ab6cfb3a1ad2fe10
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-02-10 10:16:58 -0500

    Day 8: Learning about functions with inputs

--- Day 8/Functions with Inputs/task.py
+++ Day 8/Functions with Inputs/task.py
@@ -12,3 +12,11 @@ def greet_with_name(name):
     print(f"What are you doing today {name}? ")
 
 greet_with_name("Stef")
+
+def life_in_weeks(current_age):
+    ninety_years_in_weeks = 4680
+    current_age_in_weeks = current_age * 52
+    time_left = ninety_years_in_weeks - current_age_in_weeks
+    print(f"You have {time_left} weeks left.")
+
+life_in_weeks(56)
