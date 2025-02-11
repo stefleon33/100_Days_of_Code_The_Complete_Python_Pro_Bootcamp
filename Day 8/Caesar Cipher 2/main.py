cc4de28612a360386082ed13459bbfc42acb0291
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-02-11 13:44:38 -0500

    Day 8: Part 2, combining two functions into one casear function

--- Day 8/Caesar Cipher 2/main.py
+++ Day 8/Caesar Cipher 2/main.py
@@ -4,23 +4,20 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-
 # TODO-1: Create a function called 'decrypt()' that takes 'original_text' and 'shift_amount' as inputs.
 # TODO-2: Inside the 'decrypt()' function, shift each letter of the 'original_text' *backwards* in the alphabet
 #  by the shift amount and print the decrypted text.
 # TODO-3: Combine the 'encrypt()' and 'decrypt()' functions into one function called 'caesar()'.
 #  Use the value of the user chosen 'direction' variable to determine which functionality to use.
 
-def encrypt(original_text, shift_amount):
-    cipher_text = ""
+def casear(original_text, shift_amount, encode_or_decode):
+    output_text = ""
     for letter in original_text:
+        if encode_or_decode == "decode":
+                shift_amount *= -1
         shifted_position = alphabet.index(letter) + shift_amount
         shifted_position %= len(alphabet)
-        cipher_text += alphabet[shifted_position]
-    print(f"Here is the encoded result: {cipher_text}")
+        output_text += alphabet[shifted_position]
+    print(f"Here is the {encode_or_decode}d result: {output_text}")
 
-
-encrypt(original_text=text, shift_amount=shift)
-
-
-
+casear(original_text=text, shift_amount=shift, encode_or_decode=direction)
