1d4d5f07e13c1dd4602210873bd2b8b8690220bd
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-02-12 22:00:25 -0500

    Day 9: Learning about dictionaries

--- Day 9/Dictionaries/task.py
+++ Day 9/Dictionaries/task.py
@@ -1,1 +1,24 @@
-programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected.",
+    "Function": "A piece of code that you can easily call over and over again.",
+}
+
+#print(programming_dictionary["Bug"])
+#print(programming_dictionary)
+
+
+for thing in programming_dictionary:
+    print(thing)
+
+colours = {
+    "apple": "red",
+    "pear": "green",
+    "banana": "yellow"
+}
+
+colours["grape"] = "purple"
+
+print(colours)
+for key in colours:
+    print(key)
+    print(colours[key])
