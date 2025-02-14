fd38bd2ad517af1546367b743720f008248360f8
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-02-14 10:33:07 -0500

    Day 9: Blind Auction project

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -3,4 +3,27 @@
 # TODO-3: Whether if new bids need to be added
 # TODO-4: Compare bids in dictionary
 
+from art import logo
+print(logo)
 
+bids = {}
+
+keep_bidding = True
+while keep_bidding:
+    bidder_name = input("What is your name? \n").capitalize()
+    bid_amount = int(input("What is your bid? \n$"))
+    bids[bidder_name] = bid_amount
+    keep_bidding = input("Are there any other bidders? Type 'yes' or 'no'. \n").lower()
+
+    highest_bid = max(bids.values())
+    highest_bidder = max(bids, key=bids.get)
+
+    if keep_bidding == "yes":
+         print("\n" * 100)
+    elif keep_bidding == "no":
+        keep_bidding = False
+        print(f"The winning bid is ${highest_bid} from {highest_bidder}.")
+        print(bids)
+    else:
+        print("Invalid selection. Please try again.")
+        continue
