3555cfcae2754dfb8741b8240175c20ac7a34978
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-02-17 12:14:55 -0500

    Day 10: check if a valid float is input

--- Day 10/Calculator Project/main.py
+++ Day 10/Calculator Project/main.py
@@ -1,4 +1,60 @@
+from art import logo
+
 def add(n1, n2):
     return n1 + n2
+def subtract(n1, n2):
+    return n1 - n2
+def multiply(n1, n2):
+    return n1 * n2
+def divide(n1, n2):
+    if n2 == 0:
+        print("Error: Cannot divide by zero.")
+        return None
+    return n1 / n2
+def exponent(n1, n2):
+    return n1 ** n2
 
+operations = {
+    "+" : add,
+    "-" : subtract,
+    "*" : multiply,
+    "/" : divide,
+    "**" : exponent,
+}
+def get_valid_float(prompt):
+    while True:
+        try:
+            return float(input(prompt))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
 
+def calculator():
+    keep_calculating = True
+    print(logo)
+    num1 = get_valid_float("Enter the first number: ")
+
+    while keep_calculating:
+        for symbol in operations:
+            print(symbol)
+        operation = input("Select an operation. \n")
+        if operation not in operations:
+             print("This is not a valid selection")
+             continue
+        num2 = get_valid_float("Enter the second number: ")
+        solution = operations[operation](num1, num2)
+        print(f"{num1} {operation} {num2} = {solution}")
+        if solution is None:
+            keep_calculating = False
+            print("\n" * 20)
+            calculator()
+
+        go_again = input(f"Do you want to continue with the previous result of {solution}? \n" 
+                            "Type 'y' for yes and 'n' for no. \n").lower()
+        if go_again == 'y':
+           num1 = solution
+        else:
+           keep_calculating = False
+           print("\n" * 20)
+           calculator()
+
+calculator()
