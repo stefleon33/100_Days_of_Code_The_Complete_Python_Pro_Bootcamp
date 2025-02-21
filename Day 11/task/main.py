d614bb309377aea0d113f3335cb1cb8707a9cc17
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-02-20 23:49:37 -0500

    Day 11: Blackjack Capstone Project minor changes

--- Day 11/task/main.py
+++ Day 11/task/main.py
@@ -1,16 +1,16 @@
 from art import logo
 import random
-print(logo)
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
-"""Function to adjust an 11 to a 1 to prevent the hand from going over."""
 def adjust_for_ace(hand):
+    """Function to adjust an 11 to a 1 to prevent the hand from going over."""
     while sum(hand) > 21 and 11 in hand:
         hand[hand.index(11)] = 1
 
 def play_blackjack():
     play = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ").lower()
+    print(logo)
     if play == "y":
         player_cards = []
         computer_cards = []
@@ -27,7 +27,7 @@ def play_blackjack():
         computer_score = sum(computer_cards)
 
         print(f"\tYour cards: {player_cards}, current score: {player_score}")
-        print(f"\tComputer's cards: {computer_cards}, ?")
+        print(f"\tComputer's cards: {computer_cards[0]}, ?")
 
         if player_score == 21:
             print("You win! You got 21!")
@@ -42,7 +42,7 @@ def play_blackjack():
                 player_score = sum(player_cards)
 
                 print(f"\tYour cards: {player_cards}, current score: {player_score}")
-                print(f"\tComputer's cards: {computer_cards}, ?")
+                print(f"\tComputer's cards: {computer_cards[0]}, ?")
 
                 if player_score > 21:
                     print(f"\tYour final hand: {player_cards}, final score: {player_score}")
@@ -66,20 +66,23 @@ def play_blackjack():
 
         print(f"\tYour final hand: {player_cards}, final score: {player_score}")
         print(f"\tComputer's final hand: {computer_cards}, final score: {computer_score}")
-        if player_score > 21:
-            print("You went over. You lose.")
+        if player_score == computer_score:
+            print("It's a draw!")
         elif computer_score == 21 and player_score != 21:
             print("You lose! The dealer scored 21.")
+        elif player_score > 21:
+            print("You went over. You lose.")
         elif computer_score > 21 or player_score > computer_score:
             print("You win!")
         elif player_score < computer_score:
             print("You lose.")
-        elif player_score == computer_score:
-            print("It's a draw!")
         else:
             return
+        print("\n" * 20)
         play_blackjack()
     else:
         print("Goodbye!")
+    print("\n" * 20)
+    play_blackjack()
 
 play_blackjack()
