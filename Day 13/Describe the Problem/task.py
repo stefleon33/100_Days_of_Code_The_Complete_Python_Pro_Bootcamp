85a5fe7242be9ee8f85d1699da715203d92bd9c3
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-02-27 15:15:38 -0500

    Day 13: Identifying why an error is occurring in a for loop

--- Day 13/Describe the Problem/task.py
+++ Day 13/Describe the Problem/task.py
@@ -1,5 +1,5 @@
 def my_function():
-    for i in range(1, 20):
+    for i in range(1, 21):
         if i == 20:
             print("You got it")
 
@@ -8,5 +8,8 @@ my_function()
 
 # Describe the Problem - Write your answers as comments:
 # 1. What is the for loop doing?
+"""The loop is setting the value of i to be between 1 and 19 (the end value of 20 isn't included."""
 # 2. When is the function meant to print "You got it"?
+"""When the value of i is 20."""
 # 3. What are your assumptions about the value of i?
+"""That i won't get to be 20 because it's the end value."""
