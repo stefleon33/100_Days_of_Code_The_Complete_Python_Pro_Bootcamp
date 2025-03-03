1579ec8940acc4fa7bc6b37b6f76f34ddb3722d2
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-03-03 11:44:42 -0500

    Day 13: using print to help identify errors in our code

--- Day 13/Use Print/task.py
+++ Day 13/Use Print/task.py
@@ -1,5 +1,6 @@
-word_per_page = 0
 pages = int(input("Number of pages: "))
-word_per_page == int(input("Number of words per page: "))
+print(f"pages = {pages}")
+word_per_page = int(input("Number of words per page: "))
+print(f"word_per_page = {word_per_page}")
 total_words = pages * word_per_page
 print(total_words)
