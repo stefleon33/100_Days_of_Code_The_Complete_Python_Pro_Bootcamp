b014b937fd711e5921ad8092e433af20446cd056
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-03-07 10:33:03 -0500

    Creating a game function

--- Day 14/Higher or Lower Project/main.py
+++ Day 14/Higher or Lower Project/main.py
@@ -21,3 +21,21 @@ guess = input("Who has more followers? Type 'A' or 'B': ").lower()
 # TODO: User correct, tell them their current score, move Choice B to Choice A and restart
 
 # TODO: User incorrect, end game and tell them their score.
+# TODO: User correct, tell them their current score, move Choice B to Choice A and restart
+def game():
+    while True:
+        ask_question = input("Who has more followers? Type 'A' or 'B': ").upper()
+        score = 0
+
+        if follower_count_a > follower_count_b and ask_question == 'A':
+            score += 1
+            print(f"You're right! Current Score: {score}")
+
+        elif follower_count_a < follower_count_b and ask_question == 'B':
+            score += 1
+            print(f"You're right! Current Score: {score}")
+        else:
+            print(f"You're incorrect. Final Score: {score}")
+            return False
+
+game()
